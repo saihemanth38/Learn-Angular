8251de1d077e9f1fa331fac338b35e72a97abc30
saihemanth38 <saihemanth115@gmail.com>
2019-03-14 19:10:09 +0530

    Templete driven Forms

---
+++ forms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent  {
+  name = ["banana","carrot"];
+ 
+
+log(x)
+{
+  console.log(x);
+}
+
+
+constructor() {
+
+}
+
+
+}
